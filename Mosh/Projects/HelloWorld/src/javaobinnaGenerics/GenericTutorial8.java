4b0aff80a1b1b24e617b96681f485b835ba89a52
obihov <javaobinna@gmail.com>
2022-05-07 22:57:20 -0500

    Added new GenericTutorial8 file. Generic super and extend keyword.

--- Mosh/Projects/HelloWorld/src/javaobinnaGenerics/GenericTutorial8.java
+++ Mosh/Projects/HelloWorld/src/javaobinnaGenerics/GenericTutorial8.java
@@ -2,146 +2,90 @@ package javaobinnaGenerics;
 
 public class GenericTutorial8 {
     public static void main(String[] args) {
-        /*
-        This part of the tutorial illustrates basic Generic usage.
-        The t resolves to whatever the type <> argument is.
-        You will be able to call the methods of the type.
-         */
-        Example1();
+        Product egg = new Egg("a001", 20);
+        Product pencil = new Pencil("a002", 1);
 
-        /*
-        The t resolves to the type <T> argument specified.
-        Based on OOP, you can assign a child instance to its parent type.
-        So Parent p = new Child(); and only possible because of the inheritance relationship between Parent and Child.
-         */
-        Example2();
+        Cart<Product> cart = new Cart<>(3);
+        cart.AddItemToCart(egg);
+        cart.AddItemToCart(pencil);
+        cart.PrintItemsInCart();
 
-        /*
-        Even though WildCardChild is child of WildCardParent,
-        The Generic class WildcardGen<T> where T can be WildCardChild or WildCardParent is just one class in itself.
-        And WildcardGen<WildCardChild> does not extend WildcardGen<WildCardParent>.
-        Therefore, you cannot substitute new WildcardGen<WildCardChild>() for a WildcardGen<WildCardParent>
-        in the same way we could with new WildCardChild for a  WildCardParent.
-        In Example4, you will see how using Wildcards in Generics can solve this to allow for substitution.
-         */
-        Example3();
-
-        Example4();
-
-        Example5();
+        System.out.println();
+        System.out.println("Generic Wildcard using Super to Add item. Using extend to get item as Object.");
+        AddMoreItems(cart);
+        int recentItemIndex = cart.GetTotalItemsInCart() - 1;
+        Object x = GetItem(cart, recentItemIndex);
+        if (x instanceof Product) {
+            Product product = (Product) x;
+            System.out.println("Recent item serial added: " + product.serialNumber);
+        }
     }
 
-
-
-    public static void Example1() {
-        WildcardGen<WildcardParent> parent = new WildcardGen<>();
-        parent.t.MethodParent();
-
-        WildcardGen<WildCardChild> child = new WildcardGen<>();
-        child.t.MethodChild();
-        child.t.MethodParent();
+    static void AddMoreItems(Cart<? super Product> activeCart) {
+        activeCart.AddItemToCart(new Egg("a003", 5));
+        System.out.println("New item was added.");
     }
 
-    public static void Example2() {
-        WildcardGen<WildcardParent> parent = new WildcardGen<>();
-        WildcardGen<WildCardChild> child = new WildcardGen<>();
-
-        WildcardParent parentObj = parent.t;
-        WildCardChild childObj = child.t;
-        MethodA(parentObj);
-        MethodA(childObj);
+    static Object GetItem(Cart<? super Product> cart, int index) {
+        Object x = cart.GetItemFromCart(index);
+        return x;
     }
-
-    public static void Example3() {
-        MethodB(new WildcardGen<WildcardParent>());
-        MethodC(new WildcardGen<WildCardChild>());
-
-        /*
-        Below line of code will have compile-time error because even though WildCardChild is child of WildCardParent,
-        the generic class itself "WildcardGen<WildCardChild>" is not child of WildcardGen<WildcardParent>.
-
-        MethodB(new WildcardGen<WildCardChild>());
-         */
-    }
-
-    private static void Example4() {
-        int number = 10;
-        MethodD(number);
-
-        String stringObj = "hello world";
-        MethodD(stringObj);
-
-        WildcardGen<WildcardParent> parentWildcardGen = new WildcardGen<>();
-        MethodD(parentWildcardGen);
-
-        WildcardGen<WildCardChild> cardChildWildcardGen = new WildcardGen<>();
-        MethodD(cardChildWildcardGen);
-    }
-
-    private static void Example5(){
-        /*
-        Below will throw errors because the <T> in MethodE is restricted to accept only WildcardParent or its children.
-        MethodE("hello world");
-        MethodE(false);
-         */
-
-        WildcardGen<WildcardParent> parentWildcardGen = new WildcardGen<>();
-        MethodE(parentWildcardGen);
-
-        WildcardGen<WildCardChild> cardChildWildcardGen = new WildcardGen<>();
-        MethodE(cardChildWildcardGen);
-    }
-
-
-    private static void MethodA(WildcardParent parent) { }
-    private static void MethodB(WildcardGen<WildcardParent> genObj) { }
-    private static void MethodC(WildcardGen<WildCardChild> genObj) { }
-    private static <T> void MethodD(T genObj) { }   //<T> is not restricted so can take any argument type value for genObj (e.g. string, boolean, WildCardGen, WildcardParent etc.).
-    private static <T extends WildcardParent> void MethodE(WildcardGen<T> genObj) { } //<T extends WildcardParent>, meaning T is restricted to allow only argument for genObj that is WildcardParent or its children
 
 }
 
-class WildcardGen<T extends WildcardParent> {
-    T t;
+class Cart<T extends Product> {
+    private T[] items = null;
+    private int counter = 0;
+    private int capacity = 0;
 
-    public WildcardGen() {
-        t = (T) new Object();
-    }
-}
-
-class WildcardParent {
-    int[] items = new int[5];
-    int counter;
-
-    void MethodParent() {
-        System.out.println("Parent");
+    Cart(int capacity) {
+        this.capacity = capacity;
+        this.items = (T[]) new Product[capacity];
+        //If Cart<T> is defined this way, then this.items = (T[]) new Object[capacity];
+        //But since Cart<T extends Product> is defined, then this.items = (T[]) new Product[capacity];
     }
 
-    void Add(int value){
-        if(counter > items.length){
-            System.out.println("Cannot add any more item. Maximum set to " + items.length);
+    void AddItemToCart(T item) {
+        if (counter > this.capacity) {
+            System.out.println("Total items in cart: " + this.capacity);
         }
-
-        items[counter++] = value;
-        System.out.println("Value entered.");
+        items[counter++] = item;
     }
 
-    int Get(int index){
-        var minimumIndex = 0;
-        var maximumIndex = items.length - 1;
-        if(index > items.length){
-            System.out.println("Specify an index between " +minimumIndex+ " and " +maximumIndex);
-        }
-
+    T GetItemFromCart(int index) {
         return items[index];
     }
-}
 
+    int GetTotalItemsInCart() {
+        return items.length;
+    }
 
-class WildCardChild extends WildcardParent {
-    void MethodChild() {
-        System.out.println("Child");
+    void PrintItemsInCart() {
+        for (var i : items) {
+            if(i == null) break;
+            System.out.println("Item serial number: " + i.serialNumber);
+        }
     }
 }
 
+class Product {
+    String serialNumber;
+    int weight;
 
+    Product(String serialNumber, int weight) {
+        this.serialNumber = serialNumber;
+        this.weight = weight;
+    }
+}
+
+class Egg extends Product {
+    Egg(String serialNumber, int weight) {
+        super(serialNumber, weight);
+    }
+}
+
+class Pencil extends Product {
+    Pencil(String serialNumber, int weight) {
+        super(serialNumber, weight);
+    }
+}
