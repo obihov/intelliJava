ea40b7868b86e80e24641a317921e37b6683e987
obihov <javaobinna@gmail.com>
2022-05-21 05:25:36 -0500

    Committed multiple codes

---
+++ Mosh/Projects/HelloWorld/src/javaobinnaCollections/Template.java
@@ -0,0 +1,13 @@
+package javaobinnaCollections;
+
+/** About ...
+ */
+public class Template {
+    public static void main(String[] args) {
+
+    }
+
+    public static void Example1() {
+
+    }
+}
